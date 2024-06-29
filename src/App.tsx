0824b5e98634e1a19fcfba3bfe2a2f63317fa65c
Gideon-Buba <bubaambore@gmail.com>
2024-06-29 20:02:25 +0100

    created services components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Container } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero"; // make sure to import the Hero component
 import "./App.css";
+import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
   return (
     <Container>
       <Navbar />
-      <Container>{/* Your other components go here */}</Container>
+      <Hero />
+      <About />
+      <Services />
     </Container>
   );
 }
